5ddf5da2c95b3582d2181ba3acc182c839fff9ac
William Riley-Land <william@kun.io>
2015-03-02 15:32:13 -0600

    Allow setting task dependencies.

--- index.js
+++ index.js
@@ -27,17 +27,24 @@ var fixative = module.exports = deco(function (options) {
   // #### Method to define a fixture task
   self.task = function (definition) {
     var name = definition.name;
+    var dependencies = definition.dependencies ? [].concat(definition.dependencies) : [];
 
     if (!name) throw new Error('Task name was not set.');
 
     exampleFor[name] = definition.example;
 
-    orchestrator.add(name, function (done) {
+    var task = function (callback) {
       // TODO hand off to plugin if option set
       self[name] = self.example(name);
+      debug('Set "%s."', name);
       clean.push(definition.clean);
-      done();
-    });
+      debug('Added clean up task for "%s."', name);
+      // TODO children
+      callback();
+    };
+
+    if (dependencies.length === 0) return orchestrator.add(name, task);
+    return orchestrator.add(name, dependencies, task);
   };
   // #### Method to define a helper method
   self.helper = function (name, f) {
@@ -55,22 +62,20 @@ var fixative = module.exports = deco(function (options) {
     return self;
   };
 
-  self.create = function (n, name, override, callback) {
-    if (!isReal(n)) {
-      override = name || null;
-      name = n;
-      n = 1;
-    }
+  // self.create = function (n?, name, override?, callback) {
+  self.create = function (name, callback) {
+    // if (!isReal(n)) {
+    //   override = name || null;
+    //   name = n;
+    //   n = 1;
+    // }
 
-    var f = function (done) {
-      debug('Creating %s...', name);
-      self[name] = self.example(n, name, override);
-      debug('Created %s: %o', name, self[name]);
-      // done(null);
-    };
-
-    if (callback) process.nextTick(callback);
-    return f;
+    debug('Creating "%s..."', name);
+    orchestrator.start(name, function (error) {
+      if (error) return callback(error);
+      debug('Returning created object(s)');
+      callback(null, self[name]);
+    });
   };
 
   self.example = function (n, name, override) {
